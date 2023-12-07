552523de0460523fa54e7a069ba4d33353abd8f3
jamessantiago <james.r.santiago@gmail.com>
2023-12-06 23:55:55 -0600

    old school

---
+++ csharp/Day_06.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.RegularExpressions;
+using AoCHelper;
+
+namespace adventofcode2023;
+
+public class Day_06 : BaseDay
+{
+    private readonly string[] _input;
+    private readonly List<(long time, long distance)> _races = new();
+    private readonly (long time, long distance) _race2 = new();
+
+    public Day_06()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+        var times = Regex.Matches(_input[0], @"\d+").Select(x => long.Parse(x.Value)).ToArray();
+        var distances = Regex.Matches(_input[1], @"\d+").Select(x => long.Parse(x.Value)).ToArray();
+        for (var i = 0; i < times.Length; i++)
+        {
+            _races.Add((times[i], distances[i]));
+        }
+        var times2 = Regex.Matches(_input[0].Replace(" ", ""), @"\d+").Select(x => long.Parse(x.Value)).ToArray();
+        var distances2 = Regex.Matches(_input[1].Replace(" ", ""), @"\d+").Select(x => long.Parse(x.Value)).ToArray();
+        _race2 = (times2[0], distances2[0]);
+    }
+
+    private double quad(long a, long b, long c)
+    {
+        var preRoot = b * b - 4 * a * c;
+        return (b + Math.Sqrt(preRoot)) / (2 * a);
+    }
+
+    private long quad2good(long time, long distance)
+    {
+        var min = (long)((time - Math.Sqrt(time * time - 4 * distance)) / 2);
+        //Console.WriteLine($"Min: {min}");
+        var max = (time + Math.Sqrt(time * time - 4 * distance)) / 2;
+        //Console.WriteLine($"Max: {max}");
+        return max != (long)max ? (long)(max - min) : (long)(max - min) - 1;
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        long res = 1;
+        foreach (var race in _races)
+        {
+            var r = quad2good(race.time, race.distance);
+            //Console.WriteLine($"Race results: {r}");
+            res *= r;
+        }
+
+        return new ValueTask<string>(res.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        long res = quad2good(_race2.time, _race2.distance);
+
+        return new ValueTask<string>(res.ToString());
+    }
+}
---
+++ csharp/Day_07.cs
@@ -0,0 +1,189 @@
+﻿using AoCHelper;
+
+namespace adventofcode2023;
+
+public class Day_07 : BaseDay
+{
+    List<(string hand, int bid)> hands = new();
+
+    public Day_07()
+    {
+        hands = File.ReadAllLines(InputFilePath).Select(x => (x.Split(" ")[0], int.Parse(x.Split(" ")[1]))).ToList();
+    }
+
+    List<char> strengths = new() { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
+
+    int getType(string hand)
+    {
+        if (hand.GroupBy(x => x).Count() == 5)
+        {
+            return 0;
+        }
+        else if (hand.GroupBy(x => x).Count() == 4)
+        {
+            return 1;
+        }
+        else if (hand.GroupBy(x => x).Count() == 3)
+        {
+            if (hand.GroupBy(x => x).Any(x => x.Count() == 3))
+            {
+                return 3;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+        else if (hand.GroupBy(x => x).Count() == 2)
+        {
+            if (hand.GroupBy(x => x).Any(x => x.Count() == 4))
+            {
+                return 5;
+            }
+            else
+            {
+                return 4;
+            }
+        }
+        else
+        {
+            return 6;
+        }
+    }
+
+    int compare(string hand1, string hand2)
+    {
+        try
+        {
+            for (int i = 0; i < hand1.Count(); i++)
+            {
+                if (strengths.IndexOf(hand1[i]) > strengths.IndexOf(hand2[i]))
+                {
+                    return 1;
+                }
+                else if (strengths.IndexOf(hand1[i]) < strengths.IndexOf(hand2[i]))
+                {
+                    return -1;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        return 0;
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var typedHands = hands.GroupBy(x => getType(x.hand));
+        int rank = 0;
+        long result = 0;
+        foreach (var handType in typedHands.OrderBy(x => x.Key))
+        {
+            var sortedHands = handType.ToList();
+            try
+            {
+                sortedHands.Sort((x, y) => compare(x.hand, y.hand));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{handType.Key}");
+            }
+            foreach (var hand in sortedHands)
+            {
+                rank++;
+                result += rank * hand.bid;
+                //Console.WriteLine($"{hand.hand} {hand.bid} {getType(hand.hand)}");
+            }
+        }
+        return new ValueTask<string>(result.ToString());
+    }
+
+    
+    List<char> strengths2 = new() { 'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A' };
+
+    int getType2(string hand)
+    {
+        var j = (char c) => c == 'J';
+        var nj = (char c) => c != 'J';
+        if (hand.Where(nj).GroupBy(x => x).Count() == 5)
+        {
+            return 0;
+        }
+        else if (hand.Where(nj).GroupBy(x => x).Count() == 4)
+        {
+            return 1;
+        }
+        else if (hand.Where(nj).GroupBy(x => x).Count() == 3)
+        {
+            if (hand.Where(nj).GroupBy(x => x).Any(x => x.Count(nj) + hand.Count(j) == 3))
+            {
+                return 3;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+        else if (hand.Where(nj).GroupBy(x => x).Count() == 2)
+        {
+            if (hand.Where(nj).GroupBy(x => x).Any(x => x.Count(nj) + hand.Count(j) == 4))
+            {
+                return 5;
+            }
+            else
+            {
+                return 4;
+            }
+        }
+        else
+        {
+            return 6;
+        }
+    }
+
+    int compare2(string hand1, string hand2)
+    {
+        try
+        {
+            for (int i = 0; i < hand1.Count(); i++)
+            {
+                if (strengths2.IndexOf(hand1[i]) > strengths2.IndexOf(hand2[i]))
+                {
+                    return 1;
+                }
+                else if (strengths2.IndexOf(hand1[i]) < strengths2.IndexOf(hand2[i]))
+                {
+                    return -1;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        return 0;
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var typedHands = hands.GroupBy(x => getType2(x.hand));
+        int rank = 0;
+        long result = 0;
+        foreach (var handType in typedHands.OrderBy(x => x.Key))
+        {
+            var sortedHands = handType.ToList();
+            sortedHands.Sort((x, y) => compare2(x.hand, y.hand));
+            foreach (var hand in sortedHands)
+            {
+                rank++;
+                result += rank * hand.bid;
+                //Console.WriteLine($"{hand.hand} {hand.bid} {getType2(hand.hand)}");
+            }
+        }
+        return new ValueTask<string>(result.ToString());
+    }
+}
